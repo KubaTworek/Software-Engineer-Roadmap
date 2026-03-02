521017e37ad6ec0c2cff09c22268221acbeb2c49
KubaTworek <kubatvorek@gmail.com>
2026-03-02 01:23:03 +0100

    create project

---
+++ backend-systems-lab-stage-1/src/test/java/pl/jakubtworek/backend_systems_lab_stage_1/BackendSystemsLabStage1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jakubtworek.backend_systems_lab_stage_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSystemsLabStage1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
