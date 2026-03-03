9179154e914a99bebfd8c66faa2f1a1532d9b8de
KubaTworek <kubatvorek@gmail.com>
2026-03-03 01:30:46 +0100

    cancel

---
+++ backend-systems-lab-stage-1/src/main/java/pl/jakubtworek/backend_systems_lab_stage_1/block_a/cancel/CancellableTask.java
@@ -0,0 +1,33 @@
+package pl.jakubtworek.backend_systems_lab_stage_1.block_a.cancel;
+
+public class CancellableTask implements Runnable {
+
+    /**
+     * Cooperative cancellation.
+     *
+     * Task periodically checks interruption flag.
+     * If interrupted during sleep, it restores interrupt flag
+     * and exits gracefully.
+     */
+    @Override
+    public void run() {
+
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+
+                // simulate long work
+                Thread.sleep(100);
+
+                // could also do computation here
+            }
+
+        } catch (InterruptedException e) {
+
+            // Sleep throws InterruptedException and CLEARS the flag.
+            // We restore it to preserve interruption semantics.
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println("Task stopped cooperatively.");
+    }
+}
